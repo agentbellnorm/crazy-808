3d5f0931a175fa988b30381ac331805b177f04f3
Morgan Bentell <morgan.bentell@gmail.com>
2022-09-15 21:20:10 +0200

    use create react app with typescript, can run dev and build nicely

---
+++ src/src/App.tsx
@@ -0,0 +1,32 @@
+import React, {useState} from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+// @ts-ignore
+const {invoke} = window.__TAURI__.tauri;
+// async function greet() {
+//     // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
+//     greetMsgEl.textContent = await invoke("greet", {name: greetInputEl.value});
+// }
+//
+function App() {
+    const [greeting, setGreeting] = useState<string>("");
+    const [greetingBack, setGreetingBack] = useState<string>("");
+    return (
+        <div className="App">
+            <header className="App-header">
+                <div>
+                    <input id="greet-input" value={greeting} onChange={(e) => setGreeting(e.target.value)}
+                           placeholder="Enter a name..."/>
+                    <button type="button" onClick={() => {
+                        invoke('greet', {name: greeting}).then(setGreetingBack)
+                    }}>Greet
+                    </button>
+                </div>
+                <div>{greetingBack}</div>
+            </header>
+        </div>
+    );
+}
+
+export default App;
