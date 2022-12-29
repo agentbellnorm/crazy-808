10a11d0dabc7e43d1df612e9e01d1ccb46607381
Morgan Bentell <morgan.bentell@gmail.com>
2022-12-29 16:16:54 +0100

    fancy lights on target

--- embedded-app/src/leds.rs
+++ embedded-app/src/leds.rs
@@ -1,3 +1,6 @@
+use engine::core::{get_current_bar, get_instrument};
+use engine::engine::NUMBER_OF_BARS;
+use engine::state::State;
 use linux_embedded_hal::I2cdev;
 use mcp23017::MCP23017;
 
@@ -5,6 +8,15 @@ pub const LEDS_IC2_ADDRESS: u8 = 0x20;
 
 pub struct LEDS {
     expander: MCP23017<I2cdev>,
+}
+
+fn to16arr(vec: &Vec<i32>) -> [u16; 16] {
+    let mut arr: [u16; 16] = [0; 16];
+    for i in 0..16 {
+        arr[i as usize] = *vec.get(i).unwrap() as u16;
+    }
+
+    arr
 }
 
 impl LEDS {
@@ -18,8 +30,15 @@ impl LEDS {
         LEDS { expander }
     }
 
-    pub fn light_leds(&mut self, leds: [i32; 16]) {
-        println!("light em up {:?}", leds);
+    pub fn all_on(&mut self) {
+        self.expander.write_gpioab(u16::MAX).unwrap()
+    }
+
+    pub fn all_off(&mut self) {
+        self.expander.write_gpioab(0).unwrap();
+    }
+
+    pub fn light_leds(&mut self, leds: [u16; 16]) {
         let as_number = leds
             .iter()
             .enumerate()
@@ -30,5 +49,18 @@ impl LEDS {
     pub fn light_led(&mut self, led: i32) {
         println!("lighting {}", led);
         self.expander.write_gpioab(led as u16).unwrap()
+    }
+
+    pub fn render(&mut self, state: &State) {
+        let current_bar = get_current_bar(state);
+        let active_bars = get_instrument(state);
+
+        let mut as_array = to16arr(active_bars);
+
+        // light the led of the current sixteenth
+        let actual_beat = (current_bar) % NUMBER_OF_BARS;
+        as_array[actual_beat as usize] = 1;
+
+        self.light_leds(as_array);
     }
 }
--- embedded-app/src/main.rs
+++ embedded-app/src/main.rs
@@ -7,16 +7,8 @@ use crate::leds::LEDS;
 use engine::engine::Engine;
 use engine::state::State;
 use std::sync::{Arc, Mutex};
-use std::thread;
-use std::time::Duration;
-
-fn to16arr<T>(vec: Vec<T>) -> [T; 16] {
-    vec.try_into()
-        .unwrap_or_else(|v: Vec<T>| panic!("Expected a Vec of length {} but it was {}", 4, v.len()))
-}
 
 fn main() {
-    println!("main!");
     let state = Arc::new(Mutex::new(State::initial()));
     let (engine_output_sender, engine_output_receiver) = std::sync::mpsc::sync_channel(1);
     let engine = Engine::new(state.clone(), engine_output_sender);
@@ -27,21 +19,9 @@ fn main() {
 
     loop {
         if engine_output_receiver.recv().is_ok() {
-            println!("state!");
             let arc = state.clone();
             let state = arc.lock().unwrap();
-            let i = state.selected_instrument;
-            let current = state.bar;
-            let instrument_active_bars = &state
-                .variation_a
-                .as_ref()
-                .unwrap()
-                .instrument
-                .get(i as usize)
-                .unwrap()
-                .bar;
-            let _ = leds.light_leds(to16arr(instrument_active_bars.clone()));
-            // let _ = leds.light_led(current);
+            leds.render(&state);
         }
     }
 }
